4ddb096fee291caf2f3629fddf1e8b182dcbd206
sh <sh770h@gmail.com>
2023-01-17 20:24:30 +0200

    vite 17/01/2023

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,7 +3,7 @@ element.innerHTML = `
 <header>
 <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
     <div class="container-fluid ">
-        <a class="nav-item btn btn-primary me-2 active nav-link" href="" >ונילה פרויקט</a>
+         <a class="nav-item btn btn-primary me-2 active nav-link" href="" >פרויקט ונילה</a> 
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -14,11 +14,11 @@ element.innerHTML = `
                         href="https://sh770.github.io/calc/">מחשבון</a> </li>
                 <li class="nav-item btn btn-primary me-2"> <a class="nav-link" href="">פרויקט לדוגמה</a> </li>
                 <li class="nav-item btn btn-info me-2"> <a class="nav-link" href="">פרויקט לדוגמה</a> </li>
-                <li class="nav-item btn btn-dark me-2"> <a class="nav-link" id="darkmode">דארק מוד</a></li>
+                <li class="nav-item btn btn-dark me-2"> <a class="nav-link" id="darkmode">מצב כהה</a></li>
                 <li class="nav-item btn btn-warning me-2"> <a class="nav-link"
                         href="https://sh770.github.io/progex/p19/">משחק O X</a> </li>
                 <li class="nav-item btn btn-danger me-2"> <a class="nav-link" onclick="logout()">התנתק</a> </li>
-                <li class="nav-item btn btn-secondary me-2"> <a class="nav-link" onclick="login()">לוגאין</a> </li>
+                <li class="nav-item btn btn-secondary me-2"> <a class="nav-link" onclick="login()">התחבר</a> </li>
             </ul>
         </div>
     </div>
