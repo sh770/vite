68a0effa118286fe6c24e3a6b107bf81a204d998
sh <sh770h@gmail.com>
2023-01-19 19:40:51 +0200

    vite 19/01/2023

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ document.querySelector('#app').innerHTML = `
     </a>
     <h1>Hello Vite!</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
+      <button id="counter" type="button"></button><br>
       <button id="DarkMode" type="button">כהה</button>
     </div>
     <p class="read-the-docs">
